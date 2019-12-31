81dd23765746737cb230cb09c529a4aa244f7393
Jonathan Koss <jonathan_koss@yahoo.com>
2019-12-31 22:11:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(desc="Squawk!")
-  puts desc
-  desc
+def parrot(parrot_says="Squawk!")
+  puts parrot_says
+  parrot_says
 end 
